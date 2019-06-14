48e35bdc543ffeaa89aedc8af9c559c5c301fa38
Josh McArthur <joshua.mcarthur@gmail.com>
2019-06-14 12:01:18 +1200

    Add no-op test

---
+++ test/releases_from_rss_test.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+require_relative '../releases_from_rss'
+
+module PrecompiledRubyBuilder
+  describe ReleasesFromRSS do
+
+    it 'should work' do
+      assert true
+    end
+  end
+end
